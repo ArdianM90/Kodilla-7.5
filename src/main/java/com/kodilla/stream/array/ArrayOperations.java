793abd77254869d3a78a5ceaad70437f0cfc6dbe
unknown <amienkowski@gmail.com>
2020-08-07 14:03:47 +0200

    Obliczamy średnią, funkcyjnie

--- src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,16 +1,16 @@
 package com.kodilla.stream.array;
 
+import java.util.Arrays;
 import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
         //Miało być przy użyciu IntStream i tablicy, ale tak nie było by lepiej?
-        //double avg = Arrays.stream(numbers).average().getAsDouble();
-        //return avg
+//        double avg = Arrays.stream(numbers).average().getAsDouble();
+//        return avg;
 
         IntStream.range(0, numbers.length).map(n -> numbers[n]).forEach(System.out::println);
-        OptionalDouble avg = IntStream.range(0, numbers.length).map(n -> numbers[n]).average();
-        return avg.getAsDouble();
+        return IntStream.range(0, numbers.length).map(n -> numbers[n]).average().getAsDouble();
     };
 }
